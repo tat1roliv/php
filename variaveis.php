c9d88975503c3ed3eb8fab8f7c4479d86ed4c1f4
tati oliveira <tat1roliv@gmail.com>
2022-02-03 18:29:10 -0300

    variaveis

---
+++ variaveis.php
@@ -0,0 +1,20 @@
+<?php 
+echo 'hola';
+
+$x = 10;
+
+echo $x;
+echo $x+2; 
+
+$number = 99;
+$name = 'ada';
+$booleano = true;
+$booleano2 = false;
+$vazio = null;
+$stringvazia = '';
+
+echo $number;
+echo $name;
+echo $booleano;
+echo $booleano2;
+echo $vazio;
