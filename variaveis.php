60e27a8a697c834ec0b1520db54cefa5dac522d8
tati oliveira <tat1roliv@gmail.com>
2022-02-03 20:14:38 -0300

    Update variaveis.php

--- variaveis.php
+++ variaveis.php
@@ -1,6 +1,7 @@
 <?php 
 echo 'hola';
 
+
 $x = 10;
 
 echo $x;
@@ -13,8 +14,40 @@ $booleano2 = false;
 $vazio = null;
 $stringvazia = '';
 
-echo $number;
-echo $name;
-echo $booleano;
-echo $booleano2;
-echo $vazio;
+
+echo $number.$name.$booleano; //concatenacao
+//echo $booleano2;
+//echo $vazio;
+
+$frase = " concatenando $name e $number";
+echo $frase;
+
+$x = '3';
+$y = 4;
+
+$w = $x+$y;//return 7
+
+echo $w;
+
+
+$nome = 'adinha';
+$sobrenome = 'silva';
+
+$nomeCompleto = $nome;
+$nomeCompleto .= $sobrenome; // . + - * /
+echo $nomeCompleto;
+
+
+//array
+$listaItens = [ '-item 1', '-item 2', '-item 3'];
+//echo $listaItens;
+echo $listaItens[0];
+
+
+
+
+
+
+
+
+
