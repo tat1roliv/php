b5e073a86a15914c4eae957e76e47da3b16643ef
tati oliveira <tat1roliv@gmail.com>
2022-02-11 18:00:23 -0300

    Update variaveis.php

--- variaveis.php
+++ variaveis.php
@@ -44,6 +44,36 @@ $listaItens = [ '-item 1', '-item 2', '-item 3'];
 echo $listaItens[0] ."</br>";
 
 
+$aTest = [ 'a', 'b'];
+
+$aTestSpread = [ 
+    ...$aTest,
+    'c'
+];
+echo $aTestSpread[2] ."</br>";
+print_r($aTestSpread)."</br>";
+
+
+$idade = 999;
+if($idade < 18){
+    echo  'a' ."</br>";
+} else {
+    echo 'b' ."</br>";
+}
+
+$idade2 = 9;
+echo ( $idade2 < 18 ) ? 'a' : 'b' ;
+
+$menorIdade = ( $idade2 < 18 ) ? true : false ;
+
+if($menorIdade) {
+    echo 'menor'."</br>";
+} else {
+    echo 'maior'."</br>";
+}
+
+
+
 
 
 
