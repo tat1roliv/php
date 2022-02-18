4e4c5055e907300fccbbab01dadca30c81349031
tati oliveira <tat1roliv@gmail.com>
2022-02-18 19:04:30 -0300

    Update funcoes.php

---
+++ funcoes.php
@@ -0,0 +1,24 @@
+<?php 
+
+//funcoes
+function subsequente(){
+    for ($i=0; $i<10 ; $i++){
+        echo $i . '</br>';
+    }
+}
+subsequente();
+
+
+
+$lista = [10 , 1 , 111 ];
+
+//funcoes nativas
+print_r($lista);
+
+
+
+
+
+
+
+
