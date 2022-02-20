7d2a7bbbee3aa21239e8715789a1478edb296caf
tati oliveira <tat1roliv@gmail.com>
2022-02-20 16:07:42 -0300

    Update funcoes.php

--- funcoes.php
+++ funcoes.php
@@ -10,9 +10,107 @@ subsequente();
 
 
 
-$lista = [10 , 1 , 111 ];
+
+//params
+function somar($n1, $n2){
+    $total = $n1 + $n2;
+    return $total;
+}
+echo 'total:' . somar(10,5) . '</br>' ;
+
+
+
+
+
+//params default
+function multiplicar($n1, $n2, $n3 = 1){
+    $total = $n1 * $n2 * $n3;
+    return $total;
+}
+echo 'total:' . multiplicar(10,5) . '</br>' ;
+echo 'total:' . multiplicar(10,5,2) . '</br>' ;
+
+
+
+
+
+
+//params tipado
+function multiplicarTipado(int $n1, int $n2, int $n3 = 1){
+    $total = $n1 * $n2 * $n3;
+    return $total;
+}
+echo 'total:' . multiplicar(10,5) . '</br>' ;
+echo 'total:' . multiplicar(10,5.55) . '</br>' ;
+//echo 'total:' . multiplicar(10,5.55,'ada') . '</br>' ;
+
+
+
+
+
+
+//params referencia x valor
+//valor
+echo 'total:' . somar(10,5) . '</br>' ;
+$param1 = 1;
+$param2 = 2;
+
+$somaParams =  somar($param1 , $param2);
+echo 'total:' . $somaParams . '</br>' ;
+
+//referencia
+function somarComReferencia($n1, $n2, &$totalEdit){
+    $totalEdit = $n1 + $n2;
+}
+$param1Edit = 1;
+$param2Edit = 2;
+$somaEdit = 0;
+
+$somaParamsEdit =  somar($param1Edit , $param2Edit, $somaEdit );
+echo 'total:' . $somaParamsEdit . '</br>' ;
+
+
+
+
+
+
+
+//funcao anonima
+$percentual = function(int $valor){
+    return $valor * 0.1 . '</br>';
+};
+
+echo $percentual(90);
+
+$desconto = $percentual;
+
+echo $desconto (80);
+
+
+
+
+
+
+
+//arrow function
+
+
+
+
+
+
+
+
+
+
 
 //funcoes nativas
+$lista = [10 , 1 , 111 ];
+
+print_r($lista);
+
+sort($lista);
+
 print_r($lista);
 
 
