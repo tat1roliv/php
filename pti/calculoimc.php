72ef46611947a1a99a8fa5cfe1440404d7bd73a0
tati oliveira <tat1roliv@gmail.com>
2022-02-27 16:10:27 -0300

    Update calculoimc.php

--- pti/calculoimc.php
+++ pti/calculoimc.php
@@ -12,8 +12,7 @@ $pesoInformado = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT);
 
 //FILTER_SANITIZE_NUMBER_FLOAT
 //FILTER_VALIDATE_FLOAT
-//str_replace(',', '.',string,count)
-
+//$nomeEdit = str_replace('da', 'na', $nome);
 
 function calculaIMC($altura, $peso){
     $imc = $peso / ($altura * $altura);
@@ -22,25 +21,33 @@ function calculaIMC($altura, $peso){
 
 $imcCalculado = calculaIMC($alturaInformada, $pesoInformado);
 
+function calculaFaixaImc($imc){
+
+    if ($imc <= 18.5){
+        echo 'Faixa no IMC: Magreza';
+    } else if ($imc >= 18.51 && $imc < 24.9 ){
+        echo 'Faixa no IMC: Saudável';
+    } else if ($imc >= 25.0 && $imc < 29.9 ){
+        echo 'Faixa no IMC: Sobrepeso';
+    } else if ($imc >= 30.0 && $imc < 34.9 ){
+        echo 'Faixa no IMC: Obesidade Grau I';
+    } else if ($imc >= 35.0 && $imc < 39.9 ){
+        echo 'Faixa no IMC: Obesidade Grau II';
+    } else {
+        echo 'Faixa no IMC: Obesidade Grau III';
+    }
+  
+}
+
+$imcCalculado = calculaIMC($alturaInformada, $pesoInformado);
+
+
 if($alturaInformada  && $pesoInformado ){
 
     echo 'Altura informada: ' . $alturaInformada .' metros.'.'</br>';
     echo 'Peso informado: ' . $pesoInformado .' kg.'.'</br>';
     echo 'Seu IMC calculado:'. $imcCalculado  .'</br>';
-
-    if ($imcCalculado <= 18.5){
-        echo 'Magreza';
-    } else if ($imcCalculado >= 18.51 && $imcCalculado < 24.9 ){
-        echo 'Saudável';
-    } else if ($imcCalculado >= 25.0 && $imcCalculado < 29.9 ){
-        echo 'Sobrepeso';
-    } else if ($imcCalculado >= 30.0 && $imcCalculado < 34.9 ){
-        echo 'Obesidade Grau I';
-    } else if ($imcCalculado >= 35.0 && $imcCalculado < 39.9 ){
-        echo 'Obesidade Grau II';
-    } else {
-        echo 'Obesidade Grau III';
-    }
+    calculaFaixaImc($imcCalculado);
 
 } else {
     $_SESSION['aviso'] = 'Preencha os dados corretamente! ' . '</br>';
